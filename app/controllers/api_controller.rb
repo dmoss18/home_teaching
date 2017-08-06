079e78540a46379457dbb839d41c5532b2406594
Devin Moss <devinsmoss@gmail.com>
2017-08-06 15:05:50 -0500

    add Pundit to ApiController

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -2,4 +2,13 @@ class ApiController < ActionController::API
   include Knock::Authenticable
   include Response
   include ExceptionHandler
+  include Pundit
+
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
+  private
+
+  def user_not_authorized
+    error_response("Unauthorized", 403)
+  end
 end
