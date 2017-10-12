2414290d7905270476c3e02f915cbb39106d443e
Devin Moss <devinsmoss@gmail.com>
2017-10-11 20:47:48 -0500

    Allow pundit privileges in admin pages

--- app/policies/active_admin/page_policy.rb
+++ app/policies/active_admin/page_policy.rb
@@ -13,5 +13,25 @@ module ActiveAdmin
     def show?
       true
     end
+
+    def create?
+      true
+    end
+
+    def new?
+      true
+    end
+
+    def update?
+      true
+    end
+
+    def edit?
+      true
+    end
+
+    def destroy?
+      true
+    end
   end
 end
